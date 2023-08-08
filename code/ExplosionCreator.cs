79a60264f19eeb83f8c15b13c1420d8a997c8b51
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-08-08 22:51:49 +0200

    first commit

---
+++ code/ExplosionCreator.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox.Tools;
+
+namespace Sandbox.MoskalykA
+{
+	[Library( "tool_explosioncreator", Title = "Explosion creator", Description = "Left click: Creates an explosion", Group = "fun" )]
+	public class ExplosionCreator : BaseTool
+	{
+		public override void Simulate()
+		{
+			if ( !Game.IsServer )
+				return;
+
+			if (Input.Pressed( "attack1" ))
+			{
+				var tr = DoTrace();
+
+				if ( !tr.Hit )
+					return;
+
+				var explosion = new ExplosionEntity
+				{
+					Position = tr.EndPosition
+				};
+				explosion.Explode( explosion );
+			}
+		}
+	}
+}
